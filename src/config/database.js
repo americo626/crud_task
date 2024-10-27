f8548d52ad5423c0c869fa425fa7c1cab8e8e58f
João Américo <65140042+americo626@users.noreply.github.com>
2024-10-27 20:29:29 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'postgres' // Isso indica que você está usando o PostgreSQL
+    dialect: 'postgres' 
 });
 
 const testConnection = async () => {
